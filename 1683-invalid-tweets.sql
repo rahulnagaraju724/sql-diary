45d194eb7f5d8ed053e12fa79c8d70f71329ca9a
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-06-25 20:21:37 -0500

    Create 1683-invalid-tweets.sql

---
+++ 1683-invalid-tweets.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+#1683. Invalid Tweets
+select tweet_id from tweets
+where char_length(content)>15
+#LENGTH(): returns the length of a string (in bytes).
+#CHAR_LENGTH(): return the length of a string (in characters).
