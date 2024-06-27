dab86f33c5498f1fb349483425530f62469becbe
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-06-27 17:15:40 -0500

    Create 1378-replace-employee-id-with-the-unique-identifier.sql

---
+++ 1378-replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+#1378. Replace Employee ID With The Unique Identifier
+select  e.name, euni.unique_id from employees e
+left join employeeuni as euni
+on e.id=euni.id
+#USING(id)
